557836d2071db47ac7306bcd66c2c2d0dcd3bec4
Robert Wagner <rwwagner@vt.edu>
2016-09-13 21:47:16 -0400

    Use packages

--- blueprints/ember-debonair/index.js
+++ blueprints/ember-debonair/index.js
@@ -2,13 +2,13 @@ module.exports = {
   normalizeEntityName: function() {},
 
   afterInstall: function() {
-    return this.addAddonsToProject(
-      [
+    return this.addAddonsToProject({
+      packages: [
         {
           name:'ember-cli-eslint',
           target:'3.0.0'
         }
       ]
-    );
+    });
   }
 };
