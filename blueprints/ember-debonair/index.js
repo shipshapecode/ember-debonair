f205c23b26d1a15a68271e4c9a74758a837364f7
Robert Wagner <rwwagner@vt.edu>
2016-09-13 21:57:53 -0400

    Add eslint-plugin-ship-shape to blueprint

--- blueprints/ember-debonair/index.js
+++ blueprints/ember-debonair/index.js
@@ -2,13 +2,16 @@ module.exports = {
   normalizeEntityName: function() {},
 
   afterInstall: function() {
-    return this.addAddonsToProject({
-      packages: [
-        {
-          name:'ember-cli-eslint',
-          target:'3.0.0'
-        }
-      ]
+    return this.addPackageToProject('eslint-plugin-ship-shape')
+    .then(function() {
+      return this.addAddonsToProject({
+        packages: [
+          {
+            name:'ember-cli-eslint',
+            target:'3.0.0'
+          }
+        ]
+      });
     });
   }
 };
